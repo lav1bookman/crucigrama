8ed8c33f10cefa7a91d9a35dd2c04d2a4e531fb5
Alejandro Murcia <alejadnrod14@gmail.com>
2024-06-17 20:40:03 -0500

    Adapt code to work with Roman numerals

--- src/main/java/org/example/Crucigrama.java
+++ src/main/java/org/example/Crucigrama.java
@@ -18,20 +18,20 @@ import javafx.stage.Stage;
 public class Crucigrama extends Application {
 
     // Definición de crucigramas y matrices de ocultación
-    private static char[][] crucigrama1 = {
-            {'1', '+', '2', '=', ' ', 'r', 'i', 'l', 'o'},
-            {'a', 'r', 'b', 'o', 'l', 'e', 'd', 'a', 'l'},
-            {'l', 'a', 'g', 'u', 'n', 'a', 'r', 'i', 'a'},
-            {'r', 'e', 'f', 'l', 'e', '-', 'o', 's', 'o'},
-            {'a', 'n', 'i', '+', 'l', 'o', 's', 'o', 's'},
-            {'n', 'e', 'v', 'a', 'd', 'o', 's', 'o', 'n'},
-            {'m', 'a', 'd', 'e', 'r', 'e', 'r', 'a', 'l'},
-            {'a', 'd', 'o', 'r', 'n', 'o', 's', 'e', 'r'},
-            {'l', 'o', 'c', 'o', 'c', 'e', 'n', 'a', 'd'}
+    private static String[][] crucigrama1 = {
+            {"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"},
+            {"X", "XI", "XII", "XIII", "XIV", "XV", "XVI", "XVII", "XVIII"},
+            {"XIX", "XX", "XXI", "XXII", "XXIII", "XXIV", "XXV", "XXVI", "XXVII"},
+            {"XXVIII", "XXIX", "XXX", "XXXI", "XXXII", "XXXIII", "XXXIV", "XXXV", "XXXVI"},
+            {"XXXVII", "XXXVIII", "XXXIX", "XL", "XLI", "XLII", "XLIII", "XLIV", "XLV"},
+            {"XLVI", "XLVII", "XLVIII", "XLIX", "L", "LI", "LII", "LIII", "LIV"},
+            {"LV", "LVI", "LVII", "LVIII", "LIX", "LX", "LXI", "LXII", "LXIII"},
+            {"LXIV", "LXV", "LXVI", "LXVII", "LXVIII", "LXIX", "LXX", "LXXI", "LXXII"},
+            {"LXXIII", "LXXIV", "LXXV", "LXXVI", "LXXVII", "LXXVIII", "LXXIX", "LXXX", "LXXXI"}
     };
 
     private static boolean[][] ocultas1 = {
-            {false, false, false, false, false, false, false, false, false},
+            {false, false, true, false, false, false, false, false, false},
             {false, false, false, false, false, false, false, false, false},
             {false, false, false, false, false, false, false, false, false},
             {false, false, false, false, false, false, false, false, false},
@@ -42,20 +42,20 @@ public class Crucigrama extends Application {
             {false, false, false, false, false, false, false, false, false}
     };
 
-    private static char[][] crucigrama2 = {
-            {'X', 'I', '-', 'I', 'I', '=', 'I', 'X', 'r'},
-            {'o', 'l', 'i', 'v', 'a', 'r', 'e', 'r', 'o'},
-            {'a', 'l', 'f', 'a', 'r', 'e', 'r', 'a', 's'},
-            {'n', 'e', 'g', 'o', 'c', 'i', 'o', 's', 'o'},
-            {'t', 'r', 'a', 'b', 'a', 'j', 'a', 'd', 'o'},
-            {'a', 'r', 'm', 'a', 'd', 'o', 'r', 'o', 's'},
-            {'l', 'e', 'g', 'a', 'd', 'o', 's', 'o', 's'},
-            {'a', 'b', 's', 't', 'e', 'n', 'e', 'r', 'm'},
-            {'m', 'e', 'm', 'o', 'r', 'a', 'n', 'd', 'o'}
+    private static String[][] crucigrama2 = {
+            {"LXXXII", "LXXXIII", "LXXXIV", "LXXXV", "LXXXVI", "LXXXVII", "LXXXVIII", "LXXXIX", "XC"},
+            {"XCI", "XCII", "XCIII", "XCIV", "XCV", "XCVI", "XCVII", "XCVIII", "XCIX"},
+            {"C", "CI", "CII", "CIII", "CIV", "CV", "CVI", "CVII", "CVIII"},
+            {"CIX", "CX", "CXI", "CXII", "CXIII", "CXIV", "CXV", "CXVI", "CXVII"},
+            {"CXVIII", "CXIX", "CXX", "CXXI", "CXXII", "CXXIII", "CXXIV", "CXXV", "CXXVI"},
+            {"CXXVII", "CXXVIII", "CXXIX", "CXXX", "CXXXI", "CXXXII", "CXXXIII", "CXXXIV", "CXXXV"},
+            {"CXXXVI", "CXXXVII", "CXXXVIII", "CXXXIX", "CXL", "CXLI", "CXLII", "CXLIII", "CXLIV"},
+            {"CXLV", "CXLVI", "CXLVII", "CXLVIII", "CXLIX", "CL", "CLI", "CLII", "CLIII"},
+            {"CLIV", "CLV", "CLVI", "CLVII", "CLVIII", "CLIX", "CLX", "CLXI", "CLXII"}
     };
 
     private static boolean[][] ocultas2 = {
-            {false, false, false, false, false, false, true, true, false},
+            {true, false, false, false, false, false, false, false, false},
             {false, false, true, false, false, false, false, false, false},
             {false, false, false, false, false, false, false, false, false},
             {false, false, false, false, false, false, false, false, false},
@@ -111,7 +111,7 @@ public class Crucigrama extends Application {
         vbox.setAlignment(Pos.CENTER);
         vbox.getChildren().addAll(choiceBox, root, verificarButton);
 
-        Scene scene = new Scene(vbox, 500, 500);
+        Scene scene = new Scene(vbox, 600, 600);
         primaryStage.setScene(scene);
         primaryStage.setTitle("Crucigrama");
         primaryStage.show();
@@ -120,18 +120,18 @@ public class Crucigrama extends Application {
     // Método auxiliar para crear un crucigrama en el GridPane dado
     // Este método construye dinámicamente el crucigrama seleccionado en el GridPane. Crea TextField para las
     // celdas ocultas y Label para las letras visibles.
-    private void crearCrucigrama(GridPane root, char[][] crucigrama, boolean[][] ocultas) {
+    private void crearCrucigrama(GridPane root, String[][] crucigrama, boolean[][] ocultas) {
         for (int i = 0; i < crucigrama.length; i++) {
             for (int j = 0; j < crucigrama[i].length; j++) {
                 if (ocultas[i][j]) {
                     TextField textField = new TextField();
-                    textField.setPrefWidth(30);
+                    textField.setPrefWidth(50);
                     textField.setAlignment(Pos.CENTER);
                     root.add(textField, j, i);
                 } else {
-                    Label label = new Label(String.valueOf(crucigrama[i][j]));
+                    Label label = new Label(crucigrama[i][j]);
                     label.setStyle("-fx-background-color: lightgray; -fx-border-color: black;");
-                    label.setPrefSize(30, 30);
+                    label.setPrefSize(50, 50);
                     label.setAlignment(Pos.CENTER);
                     root.add(label, j, i);
                 }
@@ -140,7 +140,7 @@ public class Crucigrama extends Application {
     }
 
     // Método para verificar las respuestas ingresadas por el usuario
-    private void verificarRespuestas(GridPane root, char[][] crucigrama1, boolean[][] ocultas1, char[][] crucigrama2, boolean[][] ocultas2) {
+    private void verificarRespuestas(GridPane root, String[][] crucigrama1, boolean[][] ocultas1, String[][] crucigrama2, boolean[][] ocultas2) {
         verificarRespuestasParaCrucigrama(root, crucigrama1, ocultas1);
         verificarRespuestasParaCrucigrama(root, crucigrama2, ocultas2);
     }
@@ -148,7 +148,7 @@ public class Crucigrama extends Application {
     // Este método verifica las respuestas ingresadas por el usuario comparando las letras ingresadas en los TextField
     // con las letras correctas del crucigrama. Cambia el color de fondo del TextField según si la respuesta es
     // correcta o incorrecta.
-    private void verificarRespuestasParaCrucigrama(GridPane root, char[][] crucigrama, boolean[][] ocultas) {
+    private void verificarRespuestasParaCrucigrama(GridPane root, String[][] crucigrama, boolean[][] ocultas) {
         for (int i = 0; i < crucigrama.length; i++) {
             for (int j = 0; j < crucigrama[i].length; j++) {
                 if (ocultas[i][j]) {
@@ -156,9 +156,9 @@ public class Crucigrama extends Application {
                     if (textField != null) {
                         String valor = textField.getText().trim();
                         if (!valor.isEmpty()) {
-                            char letraAdivinada = valor.charAt(0);
-                            char letraCorrecta = crucigrama[i][j];
-                            if (letraAdivinada == letraCorrecta) {
+                            String respuestaAdivinada = valor;
+                            String respuestaCorrecta = crucigrama[i][j];
+                            if (respuestaAdivinada.equals(respuestaCorrecta)) {
                                 ocultas[i][j] = false; // Revelar la letra en la matriz
                                 textField.setStyle("-fx-background-color: lightgreen;");
                             } else {
